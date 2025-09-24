6d6fe525e448dfd66b31eabe603d61a72fd618c5
lindanycander <76840066+lindanycander@users.noreply.github.com>
2025-09-24 15:29:11 +0200

    fix: change from workaround to disabling lint rule

--- frontend/src/styles/styled.d.ts
+++ frontend/src/styles/styled.d.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
 import "styled-components";
 import type { ThemeType } from "./theme";
 
 declare module "styled-components" {
-  export interface DefaultTheme extends Record<string, unknown>, ThemeType {}
+  export interface DefaultTheme extends ThemeType {}
 }
