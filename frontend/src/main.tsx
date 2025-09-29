86cec22d6b3db1a39ed6ae8b6c2f57de74d42599
lindanycander <76840066+lindanycander@users.noreply.github.com>
2025-09-29 17:01:53 +0200

    feat: add navigation bar without routing links

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,34 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { ThemeProvider } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme.ts";
 import {
+  AppBar,
   createTheme,
   ThemeProvider as MuiThemeProvider,
   THEME_ID,
 } from "@mui/material";
+
+const StyledAppBar = styled(AppBar)`
+  background-color: ${(props) => props.theme.colors.primary};
+  padding: ${(props) => props.theme.spacing.medium};
+  font-size: ${(props) => props.theme.fontSizes.xlarge};
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const LinkContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: ${(props) => props.theme.spacing.large};
+`;
+
+const NavigationLink = styled.span`
+  font-size: ${(props) => props.theme.fontSizes.large};
+`;
 
 const muiTheme = createTheme({});
 
@@ -15,6 +36,13 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <MuiThemeProvider theme={{ [THEME_ID]: muiTheme }}>
+        <StyledAppBar position="static">
+          WhenWhere
+          <LinkContainer>
+            <NavigationLink>My events</NavigationLink>
+            <NavigationLink>Log in/out</NavigationLink>
+          </LinkContainer>
+        </StyledAppBar>
         <App />
       </MuiThemeProvider>
     </ThemeProvider>
