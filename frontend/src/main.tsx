2674f778cdba19200c2c2fe0fbe1a68a5b0a5a41
lindanycander <76840066+lindanycander@users.noreply.github.com>
2025-09-29 16:52:19 +0200

    fix: remove emotion dependency and remove styled container for testing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,20 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme.ts";
+import {
+  createTheme,
+  ThemeProvider as MuiThemeProvider,
+  THEME_ID,
+} from "@mui/material";
+
+const muiTheme = createTheme({});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <MuiThemeProvider theme={{ [THEME_ID]: muiTheme }}>
+        <App />
+      </MuiThemeProvider>
     </ThemeProvider>
   </StrictMode>,
 );
