d5a16f0df6583c7155867eb70e57e6629ff70887
lindanycander <76840066+lindanycander@users.noreply.github.com>
2025-09-30 10:36:43 +0200

    feat: add global style to e.g. remove outer margin

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,26 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import { Root } from "./views/Root.tsx";
+import { createGlobalStyle } from "styled-components";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("Failed to find the root element");
 
+const GlobalStyle = createGlobalStyle<{ appBarHeight?: number }>`
+  *, *::before, *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    width: 100%;
+  }
+`;
+
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
+    <GlobalStyle />
     <Root />
   </BrowserRouter>,
 );
