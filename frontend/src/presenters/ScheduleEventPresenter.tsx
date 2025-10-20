1936de40305dc526dad9c6d975bbaac02b59bc88
Johan Nilsson <ni5@ug.kth.se>
2025-10-20 12:26:28 +0200

    fix: missing type

--- frontend/src/presenters/ScheduleEventPresenter.tsx
+++ frontend/src/presenters/ScheduleEventPresenter.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, type JSX } from "react";
 import { Dayjs } from "dayjs";
 import { type EventData, type EventModelType } from "../models/EventModel";
 import { ScheduleEvent } from "../views/ScheduleEvent";
